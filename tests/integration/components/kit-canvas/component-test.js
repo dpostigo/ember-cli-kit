91e5563dafed72708e1c624b84e9f9e85512f290
Dani Postigo <dani.postigo@gmail.com>
2015-07-29 19:29:37 -0400

    Finally, we setup the ember addon structure.

--- tests/integration/components/kit-canvas/component-test.js
+++ tests/integration/components/kit-canvas/component-test.js
@@ -2,7 +2,7 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 
-moduleForComponent('kit-canvas', 'Integration | Component | kit canvas', {
+moduleForComponent('/kit-canvas', 'Integration | Component | kit canvas', {
   integration: true
 });
 
