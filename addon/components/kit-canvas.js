02dcba22e76bfbcd2e48b7062c4bae5f156c72cb
Dani Postigo <dani.postigo@gmail.com>
2015-07-29 19:28:43 -0400

    stuff

---
+++ addon/components/kit-canvas.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import layout from '../templates/components/kit-canvas';
+
+export default Ember.Component.extend({
+  layout: layout
+});
--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-kit'
+  name: 'ember-cli-kit',
+  included: function(app) {
+    this._super.included(app);
+
+    // app.import(app.bowerDirectory + '/x-button/dist/js/x-button.js');
+    app.import(app.bowerDirectory + '/basscss/css/basscss.css');
+  }
 };
