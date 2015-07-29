0ebef6779c2a76d0f5b19706881f0244582744cb
Dani Postigo <dani.postigo@gmail.com>
2015-07-29 19:49:52 -0400

    stuff

--- addon/components/kit-canvas/component.js
+++ addon/components/kit-canvas/component.js
@@ -2,5 +2,7 @@ import Ember from 'ember';
 import layout from './template';
 
 export default Ember.Component.extend({
-  layout: layout
+  layout: layout,
+  classNameBindings: ['canvas'],
+  canvas: '0'
 });
