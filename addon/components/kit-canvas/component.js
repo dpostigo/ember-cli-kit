9c5ce0f0b42b8e629b3750f0716a051e08670ea9
Dani Postigo <dani.postigo@gmail.com>
2015-07-29 20:02:42 -0400

    stuff

--- addon/components/kit-canvas/component.js
+++ addon/components/kit-canvas/component.js
@@ -4,5 +4,13 @@ import layout from './template';
 export default Ember.Component.extend({
   layout: layout,
   classNameBindings: ['canvas'],
-  canvas: '0'
+  canvas: '0',
+  actions: {
+      slide: function(param) {
+      	// console.log(this.get('pages'));
+      	var index = this.get('param');
+      	this.set('canvas', 'canvas-' + index)
+      	
+      }
+  }
 });
